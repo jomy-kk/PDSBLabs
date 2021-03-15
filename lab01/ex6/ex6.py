26555a1dac87764aefc7b8ac86c8525bc5a03c89
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2021-03-15 15:44:39 +0000

    Exercise 6 - corrected notch filter

--- lab01/ex6/ex6.py
+++ lab01/ex6/ex6.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import ticker
 from pyedflib.highlevel import read_edf
 from datetime import datetime, time
-from scipy.signal import iirnotch, filtfilt
+from scipy.signal import iirnotch, filtfilt, freqz
 
 signals, signal_headers, header = read_edf('../data_Lab01/russek_rc_reduced.edf')
 duration = 320  # seconds
@@ -47,13 +48,37 @@ def get_edf(fname, channel):
         return b, sf
 
 
-fig = plt.figure(figsize=(30, 60))
-fig.suptitle("Patient: " + header['patientname'] + "         Technician: " + header['technician'] + "         Start time: " + str(start_time), fontsize=16)
+#fig = plt.figure(figsize=(30, 60))
+#fig.suptitle("Patient: " + header['patientname'] + "         Technician: " + header['technician'] + "         Start time: " + str(start_time), fontsize=16)
 n_signals = 6
 
 for i, channel in zip(range(1, n_signals+1), ('EEG C3', 'EEG C4', 'EMGQ', 'EOGE', 'ECG1', 'ECG2')):
     signal, sf = get_edf('../data_Lab01/russek_rc_reduced.edf', 'ECG2')
-    b, a = iirnotch(50/sf, 35, sf)
+    b, a = iirnotch(50/sf, 35)
+    '''
+    w, h = freqz(b, a)
+
+    fig, ax1 = plt.subplots()
+    ax1.set_title('Digital filter frequency response')
+    ax1.plot(w, 20 * np.log10(abs(h)), 'b')
+    ax1.set_ylabel('Amplitude [dB]', color='b')
+    ax1.set_xlabel('Frequency [rad/sample]')
+    ax1.grid()
+    ax2 = ax1.twinx()
+    angles = np.unwrap(np.angle(h))
+    ax2.plot(w, angles, 'g')
+    ax2.set_ylabel('Angle (radians)', color='g')
+    ax2.grid()
+    ax2.axis('tight')
+    nticks = 8
+    ax1.yaxis.set_major_locator(ticker.LinearLocator(nticks))
+    ax2.yaxis.set_major_locator(ticker.LinearLocator(nticks))
+
+    plt.show()
+    fig.savefig('notch_filter.png', bbox_inches='tight')
+    exit(0)
+    '''
+
     samples_ahead = int(time_ahead * sf)
     signal = signal[samples_ahead:int(len(signal) * timescale / duration) + samples_ahead + 1]  # trimming for the first 20 seconds (inclusive)
     filtered_signal = filtfilt(b, a, signal)
@@ -69,3 +94,5 @@ for i, channel in zip(range(1, n_signals+1), ('EEG C3', 'EEG C4', 'EMGQ', 'EOGE'
 plt.subplots_adjust(hspace=0.6)
 plt.show()
 fig.savefig("sc2.pdf", bbox_inches='tight')
+
+
