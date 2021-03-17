ab637a5f359b3ea64d6b3d6659eabf0a239af81d
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2021-03-17 23:13:47 +0000

    Fix indice 0 bug

--- lab03/py_students/get_edf.py
+++ lab03/py_students/get_edf.py
@@ -3,11 +3,11 @@ from pyedflib.highlevel import read_edf
 
 def get_edf(fname, channel):
     signals, signal_headers, header = read_edf(fname)
-    chn = 0
+    chn = -1
     for h in range(len(signal_headers)):
         if signal_headers[h]['label'] == channel:
             chn = h
-    if chn == 0:
+    if chn == -1:
         raise ValueError(channel + " cannot be found in the given EDF.")
     else:
         sf = signal_headers[chn]['sample_rate']
