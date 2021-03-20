4056c0c49ddd76d25a6e9c91d202857eef3c9eee
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2021-03-20 20:09:45 +0000

    Add Ex 2 - downsample

--- lab03/ex2/ex2.py
+++ lab03/ex2/ex2.py
@@ -1,19 +1,25 @@
 import numpy as np
 from scipy.io.wavfile import write
+from scipy.signal import resample, decimate
 import plots_aux as plt
 
+xlim = (0, 0.01)
 
-def ex2_1_generate_sinusoid(frequency=2000, duration=3, sampling_frequency=22400, label='Signal X', show=False):
+def ex2_1_generate_sinusoid(frequency=2000, duration=3, sampling_frequency=22400, label='Signal X', save=False, plot=False, show=False):
     samples = np.linspace(0, duration, int(sampling_frequency * duration), endpoint=False)
     signal = np.sin(2 * np.pi * frequency * samples)
-    write('2k.wav', sampling_frequency, signal)
-    plt.plot_signal(samples, signal, label, show=show, xlim=(0, 0.01))
+    if save:
+        write('2k.wav', sampling_frequency, signal)
+    if plot:
+        plt.plot_signal(samples, signal, label, show=show, xlim=xlim)
     return samples, signal
 
 
-def ex2_2_downsample():
-    pass
-
+def ex2_2_downsample(signal=None, ts=None, factor=2, show=False):
+    if signal is None and ts is None:
+        ts, signal = ex2_1_generate_sinusoid()
+    resampled_signal, resampled_ts = resample(signal, int(len(signal)/factor), ts)
+    plt.plot_signal_processed(ts, signal, resampled_ts, resampled_signal, 'Downsampled', 'Downsampled with factor ' + str(factor), show=show, xlim=xlim)
 
 def ex2_3_decimate():
     pass
@@ -31,4 +37,8 @@ def ex2_8_resample():
     pass
 
 
-ex2_1_generate_sinusoid(show=True)
+#ex2_1_generate_sinusoid(save=True, plot=True, show=False)
+ex2_2_downsample(factor=2, show=False)
+ex2_2_downsample(factor=4, show=False)
+ex2_2_downsample(factor=8, show=False)
+ex2_2_downsample(factor=16, show=False)
--- lab03/ex2/plots_aux.py
+++ lab03/ex2/plots_aux.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 mpl.use('macosx')
 
 
-def plot_signal(samples, signal, label, xlim=None, ylim=None, show=False):
+def plot_signal_processed(original_ts, original_signal, processed_ts, processed_signal, processing_label, label, xlim=None, ylim=None, show=False):
     fig = plt.figure(figsize=(16, 4))
     fig.tight_layout()
-    plt.plot(samples, signal)
+    plt.plot(original_ts, original_signal, '--r', label='Original')
+    plt.plot(processed_ts, processed_signal, label=processing_label)
     plt.title(label)
     plt.ylabel('Amplitude')
     plt.xlabel('Time [s]')
@@ -15,7 +16,9 @@ def plot_signal(samples, signal, label, xlim=None, ylim=None, show=False):
         plt.ylim(ylim)
     if xlim is not None:
         plt.xlim(xlim)
+    plt.legend(loc='upper right')
     fig.savefig("results/" + label + ".png", bbox_inches='tight')
     if show:
         plt.show()
+
 
