ce01cd341ebfb904ff8377ba145f5b136a26135b
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2021-03-20 20:17:28 +0000

    Fix deletion

--- lab03/ex2/plots_aux.py
+++ lab03/ex2/plots_aux.py
@@ -3,6 +3,20 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 mpl.use('macosx')
 
+def plot_signal(samples, signal, label, xlim=None, ylim=None, show=False):
+    fig = plt.figure(figsize=(16, 4))
+    fig.tight_layout()
+    plt.plot(samples, signal)
+    plt.title(label)
+    plt.ylabel('Amplitude')
+    plt.xlabel('Time [s]')
+    if ylim is not None:
+        plt.ylim(ylim)
+    if xlim is not None:
+        plt.xlim(xlim)
+    fig.savefig("results/" + label + ".png", bbox_inches='tight')
+    if show:
+        plt.show()
 
 def plot_signal_processed(original_ts, original_signal, processed_ts, processed_signal, processing_label, label, xlim=None, ylim=None, show=False):
     fig = plt.figure(figsize=(16, 4))
