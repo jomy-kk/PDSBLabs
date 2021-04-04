dc103554714c0758f31a12127f5bee85ea9a0cf3
jomy-kk <joaomiguelsaraiva@tecnico.ulisboa.pt>
2021-04-04 21:51:28 +0100

    Joao Pedro: Ex 3

--- lab04/ex3/ex3.m
+++ lab04/ex3/ex3.m
@@ -6,18 +6,18 @@ dt = 1/Fs_chirp;
 st_chirp = 3;
 t_chirp = (0:dt:st_chirp-dt)';
 
-chirp_1 = chirp(t_chirp,400,3,2000);    
+chirp_1 = chirp(t_chirp,400,3,2000);
 chirp_2 = chirp(t_chirp,2000,3,1000);
 
 chirp_3 = chirp_1+chirp_2;
 
 figure(8)
 subplot(2,2,1)
-spectrogram(chirp_3,256,0,500,Fs_chirp,'yaxis');
+spectrogram(chirp_3,256,250,500,Fs_chirp,'yaxis');
 title('Window points = 256')
 subplot(2,2,3)
-spectrogram(chirp_3,2048,0,500,Fs_chirp,'yaxis');
+spectrogram(chirp_3,2048,250,500,Fs_chirp,'yaxis');
 title('Window points = 2048')
 subplot(2,2,2)
-spectrogram(chirp_3,4096,0,500,Fs_chirp,'yaxis');
+spectrogram(chirp_3,4096,250,500,Fs_chirp,'yaxis');
 title('Window points = 4096')
